aea756eeacb35090ec8ec9e6d5458bbe9920e597
Tri Le <tri.le.mail@gmail.com>
2023-11-20 17:22:51 +0700

    Create ItemsViewModel

---
+++ AnimalCrossing/ItemsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewModel.swift
+//  AnimalCrossing
+//
+//  Created by Tri Le on 11/20/23.
+//
+
+import Foundation
+import Combine
+
+class ItemsViewModel: ObservableObject {
+    
+}
