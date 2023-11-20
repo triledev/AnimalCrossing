2e75d3240cbd21fcaba0fc5226b89c604660f2ef
Tri Le <tri.le.mail@gmail.com>
2023-11-20 16:29:57 +0700

    Define APIError

--- AnimalCrossing/APIService.swift
+++ AnimalCrossing/APIService.swift
@@ -16,4 +16,9 @@ struct APIService {
         case tops, bottoms, dresses, headwear, accessories, socks, shoes, bags
         case umbrellas, songs, recipes, fossils, construction, nookmiles, other
     }
+    
+    enum APIError: Error {
+        case unknown
+        case message(reason: String), parseError(reason: String), networkError(reason: String)
+    }
 }
