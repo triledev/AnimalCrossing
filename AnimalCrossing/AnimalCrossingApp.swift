5706853ed612b015134770b8ed6a42ba6f7c50be
Tri Le <tri.le.mail@gmail.com>
2023-11-20 14:58:31 +0700

    Initial Commit

---
+++ AnimalCrossing/AnimalCrossingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalCrossingApp.swift
+//  AnimalCrossing
+//
+//  Created by Tri Le on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimalCrossingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
